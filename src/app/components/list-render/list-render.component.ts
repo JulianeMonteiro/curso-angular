413dadc9a7d25bbc9fabcf5c274a5177725aacc6
JulianeMonteiro <juliane.a.monteiro@hotmail.com>
2023-04-03 00:18:05 -0300

    Aula: Interfaces - Angular

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from 'src/app/Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -6,33 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent {
-  animals = [
+  animals: Animal[] = [
     {
       name: 'Docinho',
       type: 'Cat',
+      age: 3,
     },
     {
       name: 'Pandora',
       type: 'Dog',
+      age: 4,
     },
     {
       name: 'Alice',
       type: 'Cat',
+      age: 9,
     },
     {
       name: 'Mel',
       type: 'Dog',
+      age: 6,
     },
     {
       name: 'Bob',
       type: 'Horse',
+      age: 4,
+    },
+    {
+      name: 'Catie',
+      type: 'Fish',
+      age: 2,
     },
   ];
 
-  animal = [
-    {
-      name: 'Alguma coisa',
-      type: 'Outra coisa',
-    },
-  ];
+  animal: Animal = {
+    name: 'Alguma coisa',
+    type: 'Outra coisa',
+    age: 13,
+  };
+
+  animalDetails = '';
+
+  showAge(animal: Animal) {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} ano(s).`;
+  }
 }
